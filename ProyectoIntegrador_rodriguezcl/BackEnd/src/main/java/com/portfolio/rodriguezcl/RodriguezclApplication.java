61669e60eae8e67ba2813c7fed2bc5112ec32a55
rodriguezcl <100380265+rodriguezcl@users.noreply.github.com>
2022-08-06 20:47:36 -0300

    -

---
+++ ProyectoIntegrador_rodriguezcl/BackEnd/src/main/java/com/portfolio/rodriguezcl/RodriguezclApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.rodriguezcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodriguezclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodriguezclApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_rodriguezcl/BackEnd/src/test/java/com/portfolio/rodriguezcl/RodriguezclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.rodriguezcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodriguezclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
